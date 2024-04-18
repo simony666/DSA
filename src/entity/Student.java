e9767c785b09799cf23cd4fd8af7e7a651f98215
Qin Long <qinlonggoh@gmail.com>
2024-04-18 17:10:18 +0800

    linkedList and studentFunction

--- src/entity/Student.java
+++ src/entity/Student.java
@@ -7,6 +7,10 @@ import adt.ListInterface;
 public class Student{
     private String studentID;
     private String studentName;
+    private int age;
+    private Programme programme;
+    private Course course;
+    private String courseRegistered;
     
     private static int nextID = 0;
     
@@ -14,8 +18,12 @@ public class Student{
     
     public Student(String studentID, String studentName) {
         this.studentID = "S" + String.format("%03d", nextID);
-        this.studentName = studentName;
         nextID++;
+        this.studentName = studentName;
+        this.age = age;
+        this.programme = programme;
+        this.course = course;
+        this.courseRegistered = courseRegistered;
     }
 
     public String getStudentID() {
@@ -41,12 +49,49 @@ public class Student{
     public void setStudentList(ListInterface<Student> studentList) {
         Student.studentList = studentList;
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Programme getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(Programme programme) {
+        this.programme = programme;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String getCourseRegistered() {
+        return courseRegistered;
+    }
+
+    public void setCourseRegistered(String courseRegistered) {
+        this.courseRegistered = courseRegistered;
+    }
+    
+    
+
+    
+    
+    
     
     @Override
     public String toString() {
-        return "Student{" + "studentID=" + studentID + ", studentName=" + studentName + '}';
+        return String.format("%-20s %-20s %-20s %-20s %-20s %-30s", studentID, studentName, age, programme.getProgName(), course.getCoverFaculty(), courseRegistered);
     }
-    
     
     
     
