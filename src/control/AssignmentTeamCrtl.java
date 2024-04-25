03de08ce24460b92c68c6b96cb574eac38b761f3
Simon Yong <51854826+simony666@users.noreply.github.com>
2024-04-26 03:05:46 +0800

    Function

--- src/control/AssignmentTeamCrtl.java
+++ src/control/AssignmentTeamCrtl.java
@@ -77,190 +77,206 @@ public class AssignmentTeamCrtl {
     public void createTeamCrtl() {
         //create New Entity, add into list
         AssignmentTeam newTeam = ui.getNewTeam();
-        if (newTeam != null){
+        if (newTeam != null) {
             addAT(newTeam);
             System.out.println("Assignment team with student limit to '" + String.valueOf(newTeam.getLimit()) + "' student created successfully.");
         }
-        
+
         //alway redirect back to main menu
         entry();
     }
+
     //case 2
     public void RemoveTeamCrtl() {
         //create New Entity, add into list
         AssignmentTeam oldTeam = ui.getRemoveTeam();
-        if (oldTeam != null){
+        if (oldTeam != null) {
             removeAT(oldTeam);
-            System.out.println("Assignment team "+oldTeam.getAssignName()+" remove successfully.");
+            System.out.println("Assignment team " + oldTeam.getAssignName() + " remove successfully.");
         }
-        
+
         //alway redirect back to main menu
         entry();
     }
-    
+
     //case 3
-    public void ModifyTeamCrtl(){
+    public void ModifyTeamCrtl() {
         ui.modifyTeam();
 
         //alway redirect back to main menu
         entry();
     }
-    
+
     //case 4
     public void AddStudentCrtl() {
         System.out.println("Please Select A Team To Add Student");
         AssignmentTeam at1 = ui.getTeam();
-        
+
         Student stu = ui.getStudent();
-        if (stu != null){
+        if (stu != null) {
             //check student course == at course
-            if (stu.getCourseList().contains(at1.getCourse())){
+            if (stu.getCourseList().contains(at1.getCourse())) {
                 at1.addStudent(stu);
                 System.out.println("Success Add Student!");
-            }else {
+            } else {
                 //else student.course not same, cancel!
                 System.out.println("Student is not under this course! Cancelled!");
             }
-            
+
         }//else student == null, user cancel, return to main menu      
-        
+
         entry();
     }
-    
+
     //case 5
     public void RemoveStudentCrtl() {
         System.out.println("Please Select A Team To Add Student");
         AssignmentTeam at1 = ui.getTeam();
-        //getStudent(Student stu);
+        if (at1 != null) {
+            ArrayList<Student> StuList = at1.getStudentList();
+            Student stu = null;
+            int choice = 0;
+            while (choice == 0) {
+                System.out.println("Student List");
+                int result = MessageUI.displayList(StuList);
+                System.out.println("Please Select A Student To Remove Or \"-1\" to remove");
+                choice = scanner.nextInt();
+                scanner.nextLine();
 
-        //ask assigment id / display all assignment if enter 999 / filter 
-        //
-        //ask student id
-        //get from getstudent controller
-        //if student exist
+                if (choice == -1) {
+                    break;
+                } else if (choice < 0 || choice > result) {
+                    MessageUI.clearScreen();
+                    MessageUI.displayInvalidChoiceMessage();
+                    choice = 0;
+                } else {
+                    stu = StuList.getEntry(choice);
+                    StuList.remove(StuList.indexOf(stu));
+                    System.out.println("Student Successfully Removed!");
+                }
+
+            }
+        }
+
+        entry();
     }
-    
-    //case 6
+
+//case 6
     public void MergeTeamCrtl() {
         System.out.println("Please Select A Team To Merge");
         AssignmentTeam at1 = ui.getTeam();
         AssignmentTeam at2 = null;
-        
+
         boolean go = false;
-        
-        if (at1 == null){
-                //exit
-                go = true;
+
+        if (at1 == null) {
+            //exit
+            go = true;
         }
-        while (!go){
+        while (!go) {
             System.out.println("Please Select A Team To Merge");
-             at2 = ui.getTeam();
-            if (at2 == null){
+            at2 = ui.getTeam();
+            if (at2 == null) {
                 //exit
                 go = true;
-            }else if (at1.equals(at2)){
+            } else if (at1.equals(at2)) {
                 MessageUI.clearScreen();
                 System.out.println("Please Select A Diffrent Assignment Team");
+            } else {
+                go = true;
             }
         }
-        
+
         //at1 and at2 is AT object need to merge
         //at1 or at2 is null == direct exit
-        if (at1 != null && at2 !=null){
+        if (at1 != null && at2 != null) {
             //need to merge, check the requirements
             //what is the requirement???
-            boolean canMerge = (at1.getTutorialGroup()==at2.getTutorialGroup() && at1.getCourse() == at2.getCourse());
-            int index = (at1.getLimit() >= at2.getLimit()?1:2);
-            AssignmentTeam mergeTeam = (at1.getLimit() >= at2.getLimit()?at1:at2);
+            boolean canMerge = (at1.getTutorialGroup() == at2.getTutorialGroup() && at1.getCourse() == at2.getCourse());
+            int index = (at1.getLimit() >= at2.getLimit() ? 1 : 2);
+            AssignmentTeam mergeTeam = (at1.getLimit() >= at2.getLimit() ? at1 : at2);
             int sCount1 = at1.getStudentCount();
             int sCount2 = at2.getStudentCount();
             int max = mergeTeam.getLimit();
-            if(!canMerge){
+            if (!canMerge) {
                 //unable to merge due to Programme or Course Not Same
                 System.out.println("Unable To Merge! Course and Programe");
-            }else if (sCount1 + sCount2 <= max){
+            } else if (sCount1 + sCount2 <= max) {
                 //able to merge
                 ArrayList<Student> sList = at1.getStudentList();
                 ArrayList<Student> s2List = at2.getStudentList();
-                for (int i = 0; i < s2List.getNumberOfEntries(); i++){
-                    if (!sList.contains(s2List.getEntry(i))){
+                for (int i = 0; i < s2List.getNumberOfEntries(); i++) {
+                    if (!sList.contains(s2List.getEntry(i))) {
                         sList.add(s2List.getEntry(i));
                     }
                 }
                 System.out.println("Success Merge Team!");
-                if (index == 1){
+                if (index == 1) {
                     //merge to at1
                     at1.setStudentList(sList);
                     removeAT(at2);
                     System.out.println(at1.toString());
-                }else{
+                } else {
                     //merge to at2
                     at2.setStudentList(sList);
                     removeAT(at1);
                     System.out.println(at2.toString());
                 }
-            }else{
+            } else {
                 //unable to merge due to exceed limit
                 System.out.println("Unable To Merge! student limit exceed!");
             }
-            
+
         }
-        
-        
-        
+
         //alway redirect back to main menu
         entry();
     }
-    
+
     //case 7
     public void ListTeamCrtl() {
         ui.displayATList();
-        
+
         System.out.println("Press Enter to continue");
         scanner.nextLine();
         MessageUI.clearScreen();
-        
-        
+
         //alway redirect back to main menu
         entry();
     }
-    
+
     //case 8
     public void ListTeamMemberCrtl() {
         AssignmentTeam at = ui.getTeam();
         ui.displayATMemberList(at);
-        
+
         System.out.println("Press Enter to continue");
         scanner.nextLine();
         MessageUI.clearScreen();
-        
-        
+
         //alway redirect back to main menu
         entry();
     }
-    
+
     //case 9
     public void ReportCrtl() {
-        
-        
+
         //alway redirect back to main menu
         entry();
     }
-    
+
     //own function
-    
-    public boolean addAT(AssignmentTeam at){
+    public boolean addAT(AssignmentTeam at) {
         return AssignmentTeamCrtl.assignmentList.add(at);
     }
-    
+
     public boolean removeAT(AssignmentTeam at) {
         var position = AssignmentTeamCrtl.assignmentList.indexOf(at);
         if (position >= 0) {
             return AssignmentTeamCrtl.assignmentList.remove(position) != null;
         }
-        return false;  
+        return false;
     }
-
 
     public LinkedList<AssignmentTeam> getAssignmentList() {
         return AssignmentTeamCrtl.assignmentList;
@@ -279,4 +295,3 @@ public class AssignmentTeamCrtl {
         //ui.displayATList();
     }
 }
-
