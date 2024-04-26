f3238675ec89257c69ec48d7d204dfca7757c2a1
AlwaysNotavailable <145825806+AlwaysNotavailable@users.noreply.github.com>
2024-04-26 21:41:33 +0800

    update UI

--- src/boundary/TutorialGroupManagementUI.java
+++ src/boundary/TutorialGroupManagementUI.java
@@ -18,20 +18,20 @@ public class TutorialGroupManagementUI {
     Scanner scanner = new Scanner(System.in);
     
 public int getTGMMenuChoice() {
-    System.out.println("╔═════════════════════════════════════════════════╗");
-    System.out.println("║     Tutorial Group Management System      ║");
-    System.out.println("╠═════════════════════════════════════════════════╣");
-    System.out.println("║ 1. Add a tutorial group to a programme");
-    System.out.println("║ 2. Remove a tutorial group from a programme");
-    System.out.println("║ 3. List all tutorial groups for a programme");
-    System.out.println("║ 4. Add students to a tutorial group");
-    System.out.println("║ 5. Remove a student from a tutorial group");
-    System.out.println("║ 6. Change the tutorial group for a student");
-    System.out.println("║ 7. List all students in a tutorial group and a programme");
-    System.out.println("║ 8. Merge tutorial groups based on criteria");
-    System.out.println("║ 9. Generate Reports");
-    System.out.println("║ 0. Quit");
-    System.out.println("╚═════════════════════════════════════════════════╝");
+    System.out.println("============================================");
+    System.out.println("||     Tutorial Group Management System       ||");
+    System.out.println("==============================================");
+    System.out.println("| 1. Add a tutorial group to a programme");
+    System.out.println("| 2. Remove a tutorial group from a programme");
+    System.out.println("| 3. List all tutorial groups for a programme");
+    System.out.println("| 4. Add students to a tutorial group");
+    System.out.println("| 5. Remove a student from a tutorial group");
+    System.out.println("| 6. Change the tutorial group for a student");
+    System.out.println("| 7. List all students in a tutorial group and a programme");
+    System.out.println("| 8. Merge tutorial groups based on criteria");
+    System.out.println("| 9. Generate Reports");
+    System.out.println("| 0. Quit");
+    System.out.println("==================================================");
     System.out.print("Enter choice (0-9): ");
 
     int choice = scanner.nextInt();
